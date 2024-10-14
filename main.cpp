bca063454867d608eedf91ef542327f6198392d6
Bogamiee <bogamie02@gmail.com>
2024-10-14 21:05:22 +0900

    Feat: Add matrix arithmetic operations

--- main.cpp
+++ main.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
 #include "Matrix.h"
+#include "MatrixCalc.h"
 using namespace std;
 
+#define fast ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0)
+
 int main() {
-    Matrix a(5);
+    fast;
+    int N;
+    cin >> N;
+
+    srand(static_cast<unsigned int>(time(NULL))); // Seed for randomness
+                                                  
+    Matrix a(N);
     a.fillMatrix(50);
     a.printMatrix();
 
-    cout << endl;
+    cout << "\n";
 
-    a.denseToSparse();
-    a.printSparseMatrix();
+    Matrix b(N);
+    b.fillMatrix(50);
+    b.printMatrix();
+
+    cout << "\n";
+
+    MatrixCalc calc(a, b);
+    Matrix c = calc.add(a, b);
+    c.printMatrix();
+
+    cout << "\n";
+
+    Matrix d = calc.sub(a, b);
+    d.printMatrix();
+
+    cout << "\n";
+
+    Matrix e = calc.mul(a, b);
+    e.printMatrix();
 
     return 0;
 }
