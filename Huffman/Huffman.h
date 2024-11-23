017c73db7a103d45daaeaf81bddd3d6edc5b8087
burstrction <150431564+minnu234oo4@users.noreply.github.com>
2024-11-24 04:57:11 +0900

    Update Huffman.h

--- Huffman/Huffman.h
+++ Huffman/Huffman.h
@@ -4,10 +4,19 @@
 #include "Node.h"
 #include "Queue.h"
 
+// Main Huffman coding function that builds the tree, encodes, and decodes a string.
 bool huffmanCoding(char str[]);
-void huffmanTree(Node** node, char str[]);
-void huffmanEncode(char str[], char encoded[], Node *huffman);
-void createCode(Node* node, char* codeMap[], char code[], int index);
-char* huffmanDecode(char encoded[], Node *huffman);
 
-#endif
+// Builds the Huffman tree from the input string.
+void huffmanTree(Node** huffman, char str[]);
+
+// Encodes the input string using the provided Huffman tree.
+void huffmanEncode(char str[], char encoded[], Node* huffman);
+
+// Recursively creates Huffman codes for all characters and stores them in the codeMap.
+void createCode(Node* node, char* codeMap[], char code[], int index);
+
+// Decodes the encoded string using the provided Huffman tree.
+char* huffmanDecode(char encoded[], Node* huffman);
+
+#endif // HUFFMAN_H
