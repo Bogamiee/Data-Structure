255542283aa0bafe785aa7273776cf0611a038e2
burstrction <150431564+minnu234oo4@users.noreply.github.com>
2024-12-01 10:18:41 +0900

    Update main.cpp

---
+++ Assignment 6/QuickSort/main.cpp
@@ -0,0 +1,33 @@
+#include "QuickSort.h"
+#include "DataGen.h"
+#include "TimeMsr.h"
+#include <iostream>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+void testQuickSort() {
+    const int numDatasets = 10;
+    const int minSize = 10000;
+    const int maxSize = 1000000;
+
+    srand(static_cast<unsigned>(time(0)));
+
+    for (int i = 0; i < numDatasets; i++) {
+        int dataSize = minSize + rand() % (maxSize - minSize + 1);
+        vector<int> data = generateRandomData(dataSize);
+
+        cout << "Dataset " << i + 1 << " (Size: " << dataSize << ") - Sorting..." << endl;
+
+        // 시간 측정
+        TimeMsr timer;
+        timer.measureExecutionTime([&]() { quickSort(data, 0, data.size() - 1); });
+        timer.printExecutionTime();
+    }
+}
+
+int main() {
+    testQuickSort();
+    return 0;
+}
